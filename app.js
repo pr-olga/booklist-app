dc5905c8dd4f3288e06cb5701f4798e63b974be1
pr-olga <frkhorkovaolga@gmail.com>
2018-12-28 21:08:17 +0100

    feat: Add class UI

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 // Book Class: Represents a Book
-
 class Book {
     constructor(title, author, isbn) {
         this.title = title;
@@ -9,10 +8,45 @@ class Book {
 }
 
 // UI Class: Handle UI Tasks
+class UI {
+    //we do not want to instantiate
+    static displayBooks() {
+        const StoredBooks = [{
+                title: 'Book One',
+                author: 'Author 1',
+                isbn: '1234'
+            },
+            {
+                title: 'Book Two',
+                author: 'Author 2',
+                isbn: '2732839'
+            }
+        ];
+        const books = StoredBooks;
+
+        books.forEach((book) => UI.addBookToList(book));
+    }
+
+    static addBookToList(book) {
+        const list = document.querySelector('#book-list');
+
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+        `;
+
+        list.appendChild(row);
+    }
+}
 
 // Store Class: Hanldes Local Storage
 
 // Event: Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 // Event: Add a Book
 
