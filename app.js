d9de6e3dfb212f29800b225b5bdb172ca74ebb22
pr-olga <frkhorkovaolga@gmail.com>
2018-12-28 21:20:14 +0100

    feat: Add event 'add a book'

--- app.js
+++ app.js
@@ -49,5 +49,18 @@ class UI {
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 // Event: Add a Book
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+    // Prevent actual submit
+    e.preventDefault();
+    // Get form values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+    //Instatiate Book
+    const book = new Book(title, author, isbn);
+
+    UI.addBookToList(book);
+});
 
 // Event: Remove a Book
