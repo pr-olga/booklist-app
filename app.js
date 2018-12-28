5d6ee68142605283b35cdacb4579813002bfab42
pr-olga <frkhorkovaolga@gmail.com>
2018-12-28 20:45:15 +0100

    feat: Add UI

---
+++ app.js
@@ -0,0 +1,11 @@
+// Book Class: Represents a Book
+
+// UI Class: Handle UI Tasks
+
+// Store Class: Hanldes Local Storage
+
+// Event: Display Books
+
+// Event: Add a Book
+
+// Event: Remove a Book
